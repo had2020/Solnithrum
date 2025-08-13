853a003087c6a73a3e5ea0ec28d7ae33db9147f1
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-08-13 00:06:27 -0700

    Update app.rs

--- Solnithrum/src/app.rs
+++ Solnithrum/src/app.rs
@@ -2,23 +2,41 @@ use yew::prelude::*;
 
 #[function_component(App)]
 pub fn app() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
+    let counter1 = use_state(|| 0);
+    let counter2 = use_state(|| 0);
+
+    let onclick1 = {
+        let counter1 = counter1.clone();
+        Callback::from(move |_| counter1.set(*counter1 + 1))
+    };
+
+    let new_wallet = {
+        let counter2 = counter2.clone();
+        Callback::from(move |_| counter2.set(*counter2 + 1))
+    };
+
+    let load_wallet = {
+        let counter2 = counter2.clone();
+        Callback::from(move |_| counter2.set(*counter2 + 1))
     };
 
     html! {
         <main>
-            <img class="logo" src="https://yew.rs/img/logo.svg" alt="Yew logo" />
             <h1>{ "Test!" }</h1>
+
             <div>
-                <button {onclick}>{ "+1" }</button>
-                <p>{ *counter }</p>
+                <button onclick={onclick1.clone()}>{ "+1" }</button>
+                <p>{ *counter1 }</p>
             </div>
+
+            <div>
+                <button onclick={new_wallet.clone()}>{ "Create Wallet" }</button>
+            </div>
+
+            <div>
+                <button onclick={load_wallet.clone()}>{ "Load Wallet" }</button>
+            </div>
+
         </main>
     }
 }
