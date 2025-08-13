21676cf9d361cd80f15f1a11f08e533788a7ac61
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-08-13 14:15:28 -0700

    updates

--- Solnithrum/src/app.rs
+++ Solnithrum/src/app.rs
@@ -1,9 +1,25 @@
+use gloo_storage::{LocalStorage, Storage};
+use web_sys::*;
 use yew::prelude::*;
+
+pub fn generate_key_pair() -> ([u8; 24], Vec<u8>) {
+    let window = web_sys::window().unwrap();
+    let crypto = window.crypto().unwrap();
+    let mut pass_phrase: [u8; 24]; // 24 words
+    crypto.get_random_values_with_u8_array(&mut pass_phrase); // Pass-Phrase
+}
+
+pub fn save_key_pair() {
+    LocalStorage::set("a1d2dr3", "value").unwrap();
+    LocalStorage::set("p4as3e", "value").unwrap();
+}
+
+pub fn load_key_pair() {}
 
 #[function_component(App)]
 pub fn app() -> Html {
-    let counter1 = use_state(|| 0);
-    let counter2 = use_state(|| 0);
+    let counter1: UseStateHandle<i32> = use_state(|| 0);
+    let counter2: UseStateHandle<i32> = use_state(|| 0);
 
     let onclick1 = {
         let counter1 = counter1.clone();
