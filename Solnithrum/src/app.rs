431f416340e50d36d52c6e9efd37bed49b4c4d8e
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-08-12 22:35:26 -0700

    updates

--- Solnithrum/src/app.rs
+++ Solnithrum/src/app.rs
@@ -2,10 +2,23 @@ use yew::prelude::*;
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
     html! {
         <main>
             <img class="logo" src="https://yew.rs/img/logo.svg" alt="Yew logo" />
             <h1>{ "Test!" }</h1>
+            <div>
+                <button {onclick}>{ "+1" }</button>
+                <p>{ *counter }</p>
+            </div>
         </main>
     }
 }
