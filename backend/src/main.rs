3ed39d2ea54d4ffad7815737bdf7f3050ddcc5a7
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-08-13 20:25:18 -0700

    updates

---
+++ backend/src/main.rs
@@ -0,0 +1,27 @@
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+//   http://127.0.0.1:8000/hello/world
+#[get("/world")]
+fn world() -> &'static str {
+    "Hello, world!"
+}
+
+//   http://127.0.0.1:8000/wave/had/100
+#[get("/<name>/<age>", rank = 2)]
+fn wave(name: &str, age: u8) -> String {
+    format!("👋 Hello, {} year old named {}!", age, name)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/hello", routes![world])
+        .mount("/wave", routes![wave])
+}
